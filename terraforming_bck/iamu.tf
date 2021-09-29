402f8a818964cd99665c985bf4d0fd5e9e01abbc
Tiziano Fogli <tiziano.fogli@flowing.it>
2021-09-29 12:52:06 +0200

    pulizia iniziale tappo1 per roba che ora non serve per esercizio terraform

--- terraforming_bck/iamu.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_user" "tappo-iam-user" {
-    name = "tappo-iam-user"
-    path = "/"
-}
-
--- terraforming_bck/snss.tf
+++
@@ -1,9 +0,0 @@
-/*
-resource "aws_sns_topic_subscription" "ddafe81f-d1bb-4488-9f67-08ea45f96e13" {
-  topic_arn                       = "arn:aws:sns:eu-west-1:755827290206:tappo1-ecs-alarms"
-  protocol                        = "email"
-  endpoint                        = "tiziano.fogli@flowing.it"
-  raw_message_delivery            = "false"
-}
-*/
-
